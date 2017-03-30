c12ebd6e971f9873d8cc94eab44ad6a95baac6aa
Sarah Stevens <sstevens2@wisc.edu>
2017-03-30 15:19:44 -0500

    Working on getting parse_phylosift_sts to use pandas, NOT FINISHED

--- parse_phylosift_sts.py
+++ parse_phylosift_sts.py
@@ -1,4 +1,4 @@
-import sys, argparse
+import sys, argparse, pandas as pd
 
 __author__ = "Sarah Stevens"
 __email__ = "sstevens2@wisc.edu"
@@ -32,114 +32,28 @@ def parseArgs():
 
 
 #Bring in the inputs and set up output
-sts_file, co_prob, co_perc, outname, concatonly = parseArgs()
-inputfile=open(sts_file, "rU")
-list1 =inputfile.readlines()
+sts_filename, co_prob, co_perc, outname, concatonly = parseArgs()
+sts_file=pd.read_table(sts_filename,header=0)
+# inputfile=open(sts_file, "rU")
+# list1 =inputfile.readlines()
 outputfile=open(outname+".prob+"+str(co_prob)+".perc"+str(co_perc)+".txt", "w")
 
 
-def prob_check(list2check): #Removes all lines below the co_prob or that have 'no rank', returns list with	all fines above or equal to the co_prob
-	outlist=[]
-	#checklist=[]
-	for line in list2check:
-		if not (float(line[5]) < co_prob):
-			if not (line[3] == 'no rank'):
-				#checklist.append(line[5])
-				outlist.append(line)
-	#print checklist
-	return outlist
-
-def match_check(list2check, rank2check): #Function for checking specific level of taxonomy for matching, returns if matches above the co_perc.  and if True, what the match is
-	ranklist = []
-	markerlist=[]
-	#adds all the lines that match the rank to ranklist
-	for line in list2check:
-		if line[3] == rank2check:
-			ranklist.append(line)
-			markerlist.append(line[-1])
-	rankcount=[]
-	#checks all possible combos for matching ranks with that line
-	for line in ranklist:
-		count=(len(line[1].split("."))/2)
-		#count=0
-		for line2 in ranklist:
-			if line != line2:
-				if line[4] == line2[4]:
-					#count+=1
-					count=count+(len(line2[1].split("."))/2)
-		rankcount.append(count)
-	num_matches= max(rankcount)
-	total=len(rankcount)
-	bm_index=rankcount.index(max(rankcount))
-	bm_name =ranklist[bm_index][4]
-	index=0
-	mmlist=[]
-	for num in rankcount:
-		if num != max(rankcount):
-			mmlist.append(ranklist[index][1])
-		index+=1
-	#print num_matches, total,  (float(num_matches)/float(total)), co_perc
-	if total <= 1:
-		return False, mmlist
-	if (float(num_matches)/float(total)) < co_perc:
-		return False, mmlist
-	else:
-		outputfile.write(rank2check+": "+bm_name+"\n")
-		print(bm_name)
-		return True, mmlist
-
-def trueMatch(matchTF): #terminates program if matchTF is false
-	if matchTF == False:
-		outputfile.close()
-		sys.exit()
-		
-def mremover(list2check, mlist): #removes markers ruled out from previous levels
-	outputlist=[]
-	for line in list2check:
-			if (line[1] not in mlist) and (line[1] not in outputlist):
-				if line not in outputlist:
-					outputlist.append(line)
-	return outputlist
-
-def checkrank(list2check, rank): #checks that all the makers have that rank, returns list of markers to remove
-	missmarklist=[]
-	rankmatchlist=[]
-	for line in list2check:
-		if (line[3] == rank):
-			rankmatchlist.append(line[1])
-	for line in list2check:
-		if (line[1] not in rankmatchlist) and (line[1] not in missmarklist):
-			missmarklist.append(line[1])
-	return missmarklist
-
-def bacterialmonly(list2check):
-	outputlist=[]
-	for line in list2check:
-		if line[-1].split("\n")[0] == "concat" or line[-1].split("\n")[0] == "16s_reps_bac":
-			outputlist.append(line)
-	return outputlist
-
-#Sort lines of inputfile into lists to be used by prob_check
-list2=[]
-for line in list1:
-	list2.append(line.split("\t"))
-inputfile.close()
-#print(list2[0])
-list2.pop(0)
-
-
-#run prob_check on the list
-prob_list=prob_check(list2)
-#print prob_list
-#run match-check on each level
+# Remove all 'no rank' and hits less than co_prob
+sts_file=sts_file[sts_file['Taxon_Rank']!='no rank']
+sts_file=sts_file[sts_file['Cumulative_Probability_Mass']>=co_prob]
+# If concatonly is true, remove all nonconcat
+if concatonly:
+    sts_file=sts_file[sts_file['Markers_Hit']=='concat']
 taxon_ranks=['superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
-for rank in taxon_ranks:
-	if len(prob_list) != 0:
-		if concatonly == True:
-			prob_list=bacterialmonly(prob_list)
-		norankmlist=checkrank(prob_list, rank)
-		prob_list=mremover(prob_list,norankmlist)
-		matchTF, mismlist =match_check(prob_list, rank)
-	if len(mismlist) != 0:
-		prob_list=mremover(prob_list, mismlist)
-	trueMatch(matchTF)
+# For each taxon rank 
+	for rank in taxon_ranks:
+	#temp subset to only that level
+	# group by Taxon names and count
+	# get the biggest taxon name
+		# if largest # hit / total > co_perc:
+			# Write name to variable/file
+		# else
+			# close file
+			# end program early
+	# remove any marker genes that didn't match the biggest taxon name found in this taxon rank
